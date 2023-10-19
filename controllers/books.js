a844fad678f831a99930d8d87684aff893a3a69e
HamdyIIbrahim <hamdyfarouk444@gmail.com>
2023-10-20 01:28:41 +0300

    add update book and delete book functions

--- controllers/books.js
+++ controllers/books.js
@@ -7,9 +7,10 @@ const getAllBooks = async (req, res) => {
     }
     res.status(200).json(books);
   } catch (error) {
-    res.status(error.statuscode || 500).json({ err: error });
+    res.status(error.statusCode || 500).json({ err: error.message });
   }
 };
+
 const createBook = async (req, res) => {
   try {
     const { title, author, ISBN, quantity, shelf_location } = req.body;
@@ -28,8 +29,41 @@ const createBook = async (req, res) => {
     }
     res.status(200).json(newBook);
   } catch (error) {
-    res.status(error.statuscode || 500).json({ err: error });
+    res.status(error.statusCode || 500).json({ err: error.message });
   }
 };
 
-module.exports = { getAllBooks, createBook };
+const updateBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const bookData = await Book.findByPk(bookId);
+    if (!bookData) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+    const validKeys = ["title", "author", "ISBN", "quantity", "shelf_location"];
+    const updatedBookData = {};
+    Object.keys(req.body).forEach((key) => {
+      if (validKeys.includes(key) && req.body[key] !== undefined) {
+        updatedBookData[key] = req.body[key];
+      }
+    });
+    const updatedBook = await bookData.update(updatedBookData);
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ err: error.message });
+  }
+};
+const deleteBook = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteBook = await Book.destroy({ where: { id: id } });
+    if (deleteBook === 0) {
+      return res.status(400).json({ error: "no book found with this id" });
+    }
+    res.status(200).json("Book deleted successfully");
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ err: error.message });
+  }
+};
+
+module.exports = { getAllBooks, createBook, deleteBook, updateBook };
--- routes/books.js
+++ routes/books.js
@@ -4,5 +4,7 @@ const bookControllers = require("../controllers/books");
 
 router.get("/get-all-books", bookControllers.getAllBooks);
 router.post("/add-book", bookControllers.createBook);
+router.put("/update-book", bookControllers.updateBook);
+router.delete("/:id", bookControllers.deleteBook);
 
 module.exports = router;
